4cbd247d65f7516821a69b43d0098c911463133a
Sondre Wold <sondrewold1@gmail.com>
2023-07-27 14:15:50 +0200

    Add bash pipeline

---
+++ prepare_all.sh
@@ -0,0 +1,17 @@
+rm -r models
+rm -r parsed_corpora
+mkdir models
+mkdir parsed_corpora
+
+for i in "es Spanish" "no Norwegian" "de German" "en English" "ru Russian" "fi Finnish" "tr Turkish" "et Estonian" "fr French" "it Italian" "hu Hungarian" "is Icelandic" "he Hebrew" "ar Arabic" "shi Tachelhit" "so Somali" "wal Wolaytta"
+do
+    set -- $i
+    echo Starting preprocessing of $2
+    echo Parsing source corpora...
+    python3 parse_source.py --code $1 --name $2
+    echo Training word2vec model...
+    python3 train_word2vec.py --corpus parsed_corpora/$2 --epochs 20 --window 5
+    echo Converting word2vec model to torch format
+    python3 convert.py $2
+    rm models/$2.model
+done
